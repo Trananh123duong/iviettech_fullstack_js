d8beda9722cbd45c97472f9a59ffc9b8ffc751fe
dev05 <dev05@dev.co-mit.com>
2025-04-25 15:55:33 +0700

    js homework chapter 4 t2

---
+++ javascript/chapter4_homework_t2/index.js
@@ -0,0 +1,61 @@
+function $(id) {
+    return document.getElementById(id);
+}
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^\d{10}$/;
+
+$("buttonRegister").onclick = function (event) {
+    event.preventDefault();
+    const userName = $("userName").value;
+    const email = $("email").value;
+    const phone = $("phone").value;
+    const sel = $("sel").value;
+    const province = $("province").value;
+    const remember = $("remember").checked;
+
+    let isValid = true;
+
+    if (!userName) {
+        isValid = false;
+        $("userName").nextElementSibling.textContent = "Vui lòng nhập họ và tên";
+        $("userName").parentElement.classList.add("has-error");
+    } else if (userName.length < 6 || userName.length > 16) {
+        isValid = false;
+        $("userName").nextElementSibling.textContent = "Họ và tên phải từ 6 đến 32 ký tự";
+        $("userName").parentElement.classList.add("has-error");
+    } else {
+        $("userName").nextElementSibling.textContent = "";
+        $("userName").parentElement.classList.remove("has-error");
+    }
+    
+    if (!email) {
+        isValid = false;
+        $("email").nextElementSibling.textContent = "Vui lòng nhập email";
+        $("email").parentElement.classList.add("has-error");
+    } else if (!emailRegex.test(email)) {
+        isValid = false;
+        $("email").nextElementSibling.textContent = "Email không hợp lệ";
+        $("email").parentElement.classList.add("has-error");
+    } else {
+        $("email").nextElementSibling.textContent = "";
+        $("email").parentElement.classList.remove("has-error");
+    }
+
+    if (!phone) {
+        isValid = false;
+        $("phone").nextElementSibling.textContent = "Vui lòng nhập số điện thoại";
+        $("phone").parentElement.classList.add("has-error");
+    } else if (!phoneRegex.test(phone)) {
+        isValid = false;
+        $("phone").nextElementSibling.textContent = "Số điện thoại phải là 10 chữ số";
+        $("phone").parentElement.classList.add("has-error");
+    } else {
+        $("phone").nextElementSibling.textContent = "";
+        $("phone").parentElement.classList.remove("has-error");
+    }
+
+    if (isValid) {
+        alert('Success')
+    }
+}
