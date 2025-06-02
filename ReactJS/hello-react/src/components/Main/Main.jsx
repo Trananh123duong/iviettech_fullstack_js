86a3a948b7991c9b8ae31287485d81ef8513d6c1
dev05 <dev05@dev.co-mit.com>
2025-06-02 09:53:52 +0700

    reactjs homeword chapter 6

--- ReactJS/hello-react/src/components/Main/Main.jsx
+++ ReactJS/hello-react/src/components/Main/Main.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import MainCss from './Main.module.css'
+import React, { useState } from 'react';
+import MainCss from './Main.module.css';
 
 const Main = ({isShowSiderBar}) => {
+  const [tabIndex, setTabIndex] = useState(1);
   return (
-    <div className={`${MainCss.main_container} ${isShowSiderBar ? MainCss.show_sidebar : ''}`}>
-      <form>
-        <div>
-          <label for="userName">UserName</label><br />
-          <input type="text" name="userName" id="userName" />
-          <span class="error"></span>
-        </div>
-        <div>
-          <label for="password">Password</label><br />
-          <input type="password" name="password" id="password" />
-          <span class="error"></span>
-        </div>
-        <div>
-          <label for="confirmPassword">Confirm Password</label><br />
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-          />
-          <span class="error"></span>
-        </div>
-        <button id="submitButton" type="button">Submit</button>
-      </form>
+    <div className={`${MainCss['tabs-container']} ${isShowSiderBar ? MainCss['show-sidebar'] : ''}`}>
+      <div className="tab-buttons">
+        <button
+          className={`${MainCss['tab-button']} ${tabIndex === 1 ? MainCss['active-tab'] : ""}`}
+          onClick={() => setTabIndex(1)}
+        >
+          Tab 1
+        </button>
+        <button
+          className={`${MainCss['tab-button']} ${tabIndex === 2 ? MainCss['active-tab'] : ""}`}
+          onClick={() => setTabIndex(2)}
+        >
+          Tab 2
+        </button>
+        <button
+          className={`${MainCss['tab-button']} ${tabIndex === 3 ? MainCss['active-tab'] : ""}`}
+          onClick={() => setTabIndex(3)}
+        >
+          Tab 3
+        </button>
+        <button
+          className={`${MainCss['tab-button']} ${tabIndex === 4 ? MainCss['active-tab'] : ""}`}
+          onClick={() => setTabIndex(4)}
+        >
+          Tab 4
+        </button>
+      </div>
+
+      <div className={MainCss['tab-content']}>
+        {tabIndex === 1 && <p>Content of Tab 1</p>}
+        {tabIndex === 2 && <p>Content of Tab 2</p>}
+        {tabIndex === 3 && <p>Content of Tab 3</p>}
+        {tabIndex === 4 && <p>Content of Tab 4</p>}
+      </div>
     </div>
   )
 }
