22a312fd3d6775f7b20303e8bd94d9d13f19e087
dev05 <dev05@dev.co-mit.com>
2025-06-19 14:06:47 +0700

    reactjs homeword chapter 9

---
+++ ReactJS/hello-react/src/themes/dark.js
@@ -0,0 +1,7 @@
+export const darkTheme = {
+  background: '#121212',
+  text: 'white',
+  tabBackground: '#333',
+  tabHover: '#555',
+  tabActive: '#1e1e1e'
+};
---
+++ ReactJS/hello-react/src/themes/light.js
@@ -0,0 +1,7 @@
+export const lightTheme = {
+  background: 'white',
+  text: 'black',
+  tabBackground: '#f5f5f5',
+  tabHover: '#e0e0e0',
+  tabActive: 'white'
+};
